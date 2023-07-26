a61fe7471315e303d84d71160da35bb9a0dfbc43
sumanChoi <voltovo@naver.com>
2023-07-26 14:37:46 +0900

    feat:비즈니스 로직 모킹

--- Chapter02/src/test/java/com/rubypaper/BoardControllerTest.java
+++ Chapter02/src/test/java/com/rubypaper/BoardControllerTest.java
@@ -1,23 +1,37 @@
 package com.rubypaper;
 
-import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import com.rubypaper.service.BoardService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class BoardControllerTest {
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BoardService boardService;
 
     @Test
     public void testHello() throws Exception{
-        String result = restTemplate.getForObject("/hello?name=suman", String.class);
-        assertEquals("Hello : suman", result);
+        when(boardService.hello("둘리")).thenReturn("Hello : 둘리");
+
+        mockMvc.perform(get("/hello").param("name", "둘리"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Hello : 둘리"))
+            .andDo(print());
     }
 }
