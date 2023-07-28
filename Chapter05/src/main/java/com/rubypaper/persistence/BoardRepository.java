969170f5afcc73ead04f3e278663d6e1b743f698
sumanChoi <voltovo@naver.com>
2023-07-28 15:08:18 +0900

    faet:쿼리 메소드 활용1

--- Chapter05/src/main/java/com/rubypaper/persistence/BoardRepository.java
+++ Chapter05/src/main/java/com/rubypaper/persistence/BoardRepository.java
@@ -3,6 +3,8 @@ package com.rubypaper.persistence;
 import com.rubypaper.domain.Board;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BoardRepository extends CrudRepository<Board, Long>{
+import java.util.List;
 
+public interface BoardRepository extends CrudRepository<Board, Long>{
+    List<Board> findByTitle(String searchKeyword);
 }
