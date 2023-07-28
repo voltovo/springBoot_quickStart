96b84c53a6523c7d0d5f632b45fd9154f53a98d5
sumanChoi <voltovo@naver.com>
2023-07-28 14:20:59 +0900

    feat:repository interface add

---
+++ Chapter05/src/main/java/com/rubypaper/persistence/BoardRepository.java
@@ -0,0 +1,8 @@
+package com.rubypaper.persistence;
+
+import com.rubypaper.domain.Board;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BoardRepository extends CrudRepository<Board, Long>{
+
+}
