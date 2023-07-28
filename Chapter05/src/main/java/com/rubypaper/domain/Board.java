8fded051172e28d3d2b530f9edaef130aa4ed339
sumanChoi <voltovo@naver.com>
2023-07-28 14:12:07 +0900

    feat:Board Entity add

---
+++ Chapter05/src/main/java/com/rubypaper/domain/Board.java
@@ -0,0 +1,26 @@
+package com.rubypaper.domain;
+
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Board {
+    @Id @GeneratedValue
+    private Long seq;
+    private String title;
+    private String writer;
+    private String content;
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date createDate;
+    private Long cnt;
+}
