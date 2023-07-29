a2a3b94b7ff9f81fb7dee0de839c7ea480a3f552
sumanChoi <voltovo@naver.com>
2023-07-29 16:16:49 +0900

    feat:index and login page add

---
+++ Chapter06/src/main/java/com/rubypaper/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.rubypaper.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public void loginView(){
+
+    }
+}
