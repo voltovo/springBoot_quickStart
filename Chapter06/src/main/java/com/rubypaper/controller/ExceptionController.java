45a85d7fe387007ae5bcdcab7463a97992bef671
sumanChoi <voltovo@naver.com>
2023-07-30 13:10:13 +0900

    feat:예외처리 추가 - boardException 구현 완료 - Exception 예외처리 미 구현... 에러 내용 확인 필요

--- Chapter06/src/main/java/com/rubypaper/controller/ExceptionController.java
+++ Chapter06/src/main/java/com/rubypaper/controller/ExceptionController.java
@@ -1,21 +1,24 @@
 package com.rubypaper.controller;
 
-import com.rubypaper.exception.BoardException;
-import com.rubypaper.exception.BoardNotFoundException;
+import java.sql.SQLException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.sql.SQLException;
+import com.rubypaper.exception.BoardNotFoundException;
 
 @Controller
 public class ExceptionController {
+
     @RequestMapping("/boardError")
-    public String boardError(){
+    public String boardError() {
         throw new BoardNotFoundException("검색된 게시글이 없습니다.");
     }
 
     @RequestMapping("/illegalArgumentError")
-    public String ellegalArgumentError(){
+    public String ellegalArgumentError() {
         throw new IllegalArgumentException("부적절한 인자가 전달되었습니다.");
     }
 
@@ -23,4 +26,11 @@ public class ExceptionController {
     public String sqlError() throws SQLException {
         throw new SQLException("SQL 구문에 오류가 있습니다.");
     }
+
+    @ExceptionHandler(SQLException.class)
+    public String numberFormatError(SQLException exception, Model model) {
+        model.addAttribute("exception", exception);
+        return "/errors/sqlError";
+    }
+
 }
