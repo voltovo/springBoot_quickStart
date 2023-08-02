72ebe4935774c5875a5b8c7b1db544234f58d8b6
sumanChoi <voltovo@naver.com>
2023-08-02 19:19:10 +0900

    feat:게시글 등록, 수정, 삭제 구현

--- BoardWeb/src/main/java/com/rubypaper/board/controller/BoardController.java
+++ BoardWeb/src/main/java/com/rubypaper/board/controller/BoardController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,5 +28,28 @@ public class BoardController {
     public String getBoard(Board board, Model model){
         model.addAttribute("board", boardService.getBoard(board));
         return "board/getBoard";
+    }
+
+    @GetMapping ("/insertBoard")
+    public String insertBoardView(){
+        return "board/insertBoard";
+    }
+
+    @PostMapping("/insertBoard")
+    public String insertBoard(Board board){
+        boardService.insertBoard(board);
+        return "redirect:getBoardList";
+    }
+
+    @PostMapping("/updateBoard")
+    public String updateBoard(Board board){
+        boardService.updateBoard(board);
+        return "forward:getBoardList";
+    }
+
+    @GetMapping("/deleteBoard")
+    public String deleteBoard(Board board){
+        boardService.deleteBoard(board);
+        return "forward:getBoardList";
     }
 }
