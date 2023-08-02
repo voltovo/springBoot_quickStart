473411ab9caa8f18fd0b8bca334a35c84c0474d7
sumanChoi <voltovo@naver.com>
2023-08-02 16:11:12 +0900

    feat:게시판 서비스 인터페이스 add

---
+++ BoardWeb/src/main/java/com/rubypaper/board/service/BoardServiceImpl.java
@@ -0,0 +1,46 @@
+package com.rubypaper.board.service;
+
+import com.rubypaper.board.domain.Board;
+import com.rubypaper.board.persistence.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardServiceImpl implements BoardService{
+    @Autowired
+    private BoardRepository boardRepo;
+
+    @Override
+    public void insertBoard(Board board){
+        boardRepo.save(board);
+    }
+
+    @Override
+    public void updateBoard(Board board) {
+        Board findBoard = boardRepo.findById(board.getSeq()).get();
+
+        findBoard.setTitle(board.getTitle());
+        findBoard.setContent(board.getContent());
+        boardRepo.save(findBoard);
+    }
+
+    @Override
+    public void deleteBoard(Board board) {
+        boardRepo.deleteById(board.getSeq());
+    }
+
+    @Override
+    public Board getBoard(Board board) {
+        return boardRepo.findById(board.getSeq()).get();
+    }
+
+    @Override
+    public Page<Board> getBoardList(Board board) {
+        Pageable pageable = PageRequest.of(0, 10, Sort.Direction.DESC, "seq");
+        return boardRepo.getBoardList(pageable);
+    }
+}
