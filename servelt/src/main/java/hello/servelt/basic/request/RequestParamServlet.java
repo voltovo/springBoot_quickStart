30fa54efc432c9c7fbaa424b6ed0b8bdb35619e9
sumanChoi <voltovo@naver.com>
2023-10-12 21:25:37 +0900

    feat:get query parameter 예제1

---
+++ servelt/src/main/java/hello/servelt/basic/request/RequestParamServlet.java
@@ -0,0 +1,22 @@
+package hello.servelt.basic.request;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("RequestParamServlet.service");
+        System.out.println("전체 파라미터 조회 start");
+        request.getParameterNames().asIterator()
+            .forEachRemaining(paramName -> System.out.println(paramName + "= " + request.getParameter(paramName)));
+        System.out.println("전체 파라미터 조회 start");
+    }
+}
