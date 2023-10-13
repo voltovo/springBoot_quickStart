2d2671f3b565db0487af26dcc8e082418a1c7592
sumanChoi <voltovo@naver.com>
2023-10-13 21:18:29 +0900

    feat:API 메시지 바디 _ 단순 텍스트 예제

---
+++ servelt/src/main/java/hello/servelt/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,26 @@
+package hello.servelt.basic.request;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.util.StreamUtils;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 메시지 바디의 내용을 byte 코드로 바로 얻을 수 있다.
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
+    }
+}
