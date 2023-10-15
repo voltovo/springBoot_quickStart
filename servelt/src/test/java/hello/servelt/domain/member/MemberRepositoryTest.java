ae155c96f7b8f57bfb0dad1b9111096cc5d3a0c7
sumanChoi <voltovo@naver.com>
2023-10-15 22:44:40 +0900

    feat:memberRepository test add

--- servelt/src/test/java/hello/servelt/domain/member/MemberRepositoryTest.java
+++ servelt/src/test/java/hello/servelt/domain/member/MemberRepositoryTest.java
@@ -1,4 +1,42 @@
-import static org.junit.jupiter.api.Assertions.*;
+package hello.servelt.domain.member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
 class MemberRepositoryTest {
-  
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member("suman", 30);
+        //when
+        Member saveMember = memberRepository.save(member);
+        //then
+        Member findMember = memberRepository.findById(saveMember.getId());
+        assertThat(findMember).isEqualTo(saveMember);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 33);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //when
+        List<Member> memberList = memberRepository.findByAll();
+        //then
+        assertThat(memberList.size()).isEqualTo(2);
+        assertThat(memberList).contains(member1, member2);
+    }
 }
