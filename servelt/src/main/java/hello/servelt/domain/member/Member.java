cd18ae162804e0331674ee4f1c4e3ca502d58bb4
sumanChoi <voltovo@naver.com>
2023-10-15 22:35:00 +0900

    feat:Member, MemberRepository add

---
+++ servelt/src/main/java/hello/servelt/domain/member/Member.java
@@ -0,0 +1,22 @@
+package hello.servelt.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Member {
+    private long id;
+    private String username;
+    private int age;
+
+    public Member() {
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
