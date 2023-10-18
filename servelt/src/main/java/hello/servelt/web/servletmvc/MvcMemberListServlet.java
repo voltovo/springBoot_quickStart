275649424832e634b5d2c8448d90f04fd51d7930
sumanChoi <voltovo@naver.com>
2023-10-18 20:19:51 +0900

    feat:mvc 패턴을 적용한 회원 애플리케이션 예제

---
+++ servelt/src/main/java/hello/servelt/web/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,28 @@
+package hello.servelt.web.servletmvc;
+
+import hello.servelt.domain.member.Member;
+import hello.servelt.domain.member.MemberRepository;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findByAll();
+        //Model에 데이터를 보관한다.
+        request.setAttribute("members", members);
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
