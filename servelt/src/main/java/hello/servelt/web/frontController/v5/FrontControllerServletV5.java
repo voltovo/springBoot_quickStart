2f3f12a18aa918dd8d10d99ac488d9e12aaac0ad
sumanChoi <voltovo@naver.com>
2023-10-22 18:14:33 +0900

    feat:v5에서 v4 controller 사용하기 예제2

--- servelt/src/main/java/hello/servelt/web/frontController/v5/FrontControllerServletV5.java
+++ servelt/src/main/java/hello/servelt/web/frontController/v5/FrontControllerServletV5.java
@@ -5,7 +5,11 @@ import hello.servelt.web.frontController.MyView;
 import hello.servelt.web.frontController.v3.controller.MemberFormControllerV3;
 import hello.servelt.web.frontController.v3.controller.MemberListControllerV3;
 import hello.servelt.web.frontController.v3.controller.MemberSaveControllerV3;
+import hello.servelt.web.frontController.v4.controller.MemberFormControllerV4;
+import hello.servelt.web.frontController.v4.controller.MemberListControllerV4;
+import hello.servelt.web.frontController.v4.controller.MemberSaveControllerV4;
 import hello.servelt.web.frontController.v5.adapter.ControllerV3HandlerAdapter;
+import hello.servelt.web.frontController.v5.adapter.ControllerV4HandlerAdapter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -29,13 +33,19 @@ public class FrontControllerServletV5 extends HttpServlet {
     }
 
     private void initHandlerMappingMap() {
+        // V3
         handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+        // V4
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
 
     private void initHandlerAdapters() {
         handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
 
     @Override
