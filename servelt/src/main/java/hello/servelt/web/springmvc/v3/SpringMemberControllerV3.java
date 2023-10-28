731637aa9e387c4dab39f7cfabdd7a4a53ad96d5
sumanChoi <voltovo@naver.com>
2023-10-28 10:29:13 +0900

    feat:spring mvc pattern 실용적인 방식_method 추가

--- servelt/src/main/java/hello/servelt/web/springmvc/v3/SpringMemberControllerV3.java
+++ servelt/src/main/java/hello/servelt/web/springmvc/v3/SpringMemberControllerV3.java
@@ -5,6 +5,8 @@ import hello.servelt.domain.member.MemberRepository;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,12 +16,14 @@ public class SpringMemberControllerV3 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/new-form")
+    //@RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping("/new-form")
     public String newForm() {
         return "new-form";
     }
 
-    @RequestMapping("/save")
+    //@RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
     public String save(@RequestParam("username") String username, @RequestParam("age") int age, Model model) {
         Member member = new Member(username, age);
         memberRepository.save(member);
@@ -30,7 +34,8 @@ public class SpringMemberControllerV3 {
     }
 
     // /springmvc/v3/members
-    @RequestMapping
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String members(Model model) {
         List<Member> members = memberRepository.findByAll();
         //Model에 데이터를 보관한다.
