3529a32f7548fe578b9eba9f46653a795adeccb8
sumanChoi <voltovo@naver.com>
2023-10-31 20:17:51 +0900

    feat:spring mvc project init

---
+++ HelloMvc/src/main/java/hello/springmvc/HelloMvcApplication.java
@@ -0,0 +1,13 @@
+package hello.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMvcApplication.class, args);
+    }
+
+}
