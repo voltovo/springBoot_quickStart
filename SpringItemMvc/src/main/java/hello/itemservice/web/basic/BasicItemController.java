86cd124788057205103d1870874e83e39a056941
sumanChoi <voltovo@naver.com>
2023-11-06 16:55:22 +0900

    feat:itemList page에 thymleaf 적용:

---
+++ SpringItemMvc/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,31 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> itemList = itemRepository.findAll();
+        model.addAttribute("items", itemList);
+        return "basic/items";
+    }
+
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 5));
+    }
+}
