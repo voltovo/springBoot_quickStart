f1ca8d34a52fe629c2b7fd4a7e63a115bb85effc
sumanChoi <voltovo@naver.com>
2023-11-07 19:36:43 +0900

    feat:상품 등록 처리 예제

--- SpringItemMvc/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ SpringItemMvc/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -9,7 +9,10 @@ import org.springframework.boot.Banner.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -32,8 +35,21 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
     @PostConstruct
-    public void init(){
+    public void init() {
+
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 5));
     }
