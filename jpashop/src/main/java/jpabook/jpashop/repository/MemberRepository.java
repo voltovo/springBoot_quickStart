26f391a72808e9fca2b9ce8ada49635b9f640ea6
sumanChoi <voltovo@naver.com>
2023-11-17 23:01:17 +0900

    feat:회원 서비스 개발

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -2,28 +2,46 @@ package jpabook.jpashop.repository;
 
 import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
+    /**
+     * 회원 저장
+     * @param member
+     */
     public void save(Member member) {
         em.persist(member);
     }
 
+    /**
+     * 회원 상세 조회
+     * @param id
+     * @return
+     */
     public Member findOne(Long id) {
         return em.find(Member.class, id);
     }
 
+    /**
+     * 회원 목록 조회
+     * @return
+     */
     public List<Member> findAll() {
         return em.createQuery("select m from Member m", Member.class).getResultList();
     }
 
+    /**
+     * 이름으로 회원 조회
+     * @param name
+     * @return
+     */
     public List<Member> findByName(String name) {
         return em.createQuery("select m from Member m where m.name = :name", Member.class)
             .setParameter("name", name).getResultList();
---
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,54 @@
+package jpabook.jpashop.service;
+
+import java.util.List;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true) // 읽기 전용, 쓰기 메소드에만 별도 Transactional 표기
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional // 쓰기인 경우 별도 표기
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 회원 목록 조회
+     * @return List<Member>
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 상세 조회
+     * @param memberId
+     * @return Member
+     */
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+    /**
+     * 중복 회원 검증
+     * @param member
+     */
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+    }
+}
