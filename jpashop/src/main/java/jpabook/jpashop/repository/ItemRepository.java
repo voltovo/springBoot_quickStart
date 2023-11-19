070cb4a0a316c5fe5d0db14c4c57c04e85b45173
sumanChoi <voltovo@naver.com>
2023-11-19 23:13:11 +0900

    feat:상품 레파지토리 추가

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.repository;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+    private final EntityManager em;
+
+    /**
+     * 상품 저장
+     * @param item
+     */
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        }else{
+            em.merge(item);
+        }
+    }
+
+    /**
+     * 상품 조회
+     * @param id
+     * @return
+     */
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    /**
+     * 상품 전체 조회
+     * @return
+     */
+    public List<Item> findAll(){
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}
