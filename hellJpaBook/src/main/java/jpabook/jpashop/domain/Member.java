db3ec50bc23416383b450d62f1f674256eb9c97f
sumanChoi <voltovo@naver.com>
2023-12-06 21:33:20 +0900

    feat:entity mapping

--- hellJpaBook/src/main/java/jpabook/jpashop/domain/Member.java
+++ hellJpaBook/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,59 @@
 package jpabook.jpashop.domain;
 
-public class Member {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.jboss.logging.MessageLogger;
 
+@Entity
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "MEMBER_ID")
+    private Long id;
+    private String name;
+    private String city;
+    private String street;
+    private String zipcoe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcoe() {
+        return zipcoe;
+    }
+
+    public void setZipcoe(String zipcoe) {
+        this.zipcoe = zipcoe;
+    }
 }
--- hellJpaBook/src/main/java/jpabook/jpashop/domain/Order.java
+++ hellJpaBook/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,5 +1,57 @@
 package jpabook.jpashop.domain;
 
-public class Order {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ORDER_ID")
+    private Long id;
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+    private LocalDateTime orderDate;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getDate() {
+        return orderDate;
+    }
+
+    public void setDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
