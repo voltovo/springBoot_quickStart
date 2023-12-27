db7bd6cf547c2f4303c0324e985c03aff6ba94b3
sumanChoi <voltovo@naver.com>
2023-12-27 19:54:38 +0900

    feat:회원 등록 api v2

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -13,10 +13,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
+
+    /**
+     * 회원 등록 v1
+     * api 스팩과 엔티티가 같으면 엔티티를 수정하거나
+     * api 스팩이 변경된 경우 사이드 이팩트가 발생할 수 있다
+     * @param member
+     * @return
+     */
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
         Long joinId = memberService.join(member);
         return new CreateMemberResponse(joinId);
+    }
+
+    /**
+     * 회원 등록 v2
+     * api 스팩을 받는 객체를 하나 만들어서 전달하는 DTO 생성
+     * api 스팩이나 엔티티가 변경되어도 대응이 간편하다
+     * @param request
+     * @return
+     */
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long joinId = memberService.join(member);
+
+        return new CreateMemberResponse(joinId);
+    }
+
+    @Data
+    static class CreateMemberRequest{
+        private String name;
     }
 
     @Data
