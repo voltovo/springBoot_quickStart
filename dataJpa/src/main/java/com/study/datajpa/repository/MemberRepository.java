6cb5a59df67ded39ad40dea21c0399c89aea82c0
sumanChoi <voltovo@naver.com>
2024-02-14 15:24:58 +0900

    feat:벌크성 수정 쿼리 spring Jpa 적용

--- dataJpa/src/main/java/com/study/datajpa/repository/MemberRepository.java
+++ dataJpa/src/main/java/com/study/datajpa/repository/MemberRepository.java
@@ -6,11 +6,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query(value = "select m from Member m left join m.team t",
         countQuery = "select count(m) from Member m")
     Page<Member> findByAge(int age, PageRequest pageRequest);
+
+    @Modifying
+    @Query("update Member m set m.age = m.age+1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
 }
--- dataJpa/src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
+++ dataJpa/src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
@@ -88,4 +88,20 @@ class MemberRepositoryTest {
 
      
      }
+
+    @Test
+    public void bulkUpdate() throws Exception {
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 19));
+        memberRepository.save(new Member("member3", 20));
+        memberRepository.save(new Member("member4", 21));
+        memberRepository.save(new Member("member5", 40));
+        memberRepository.save(new Member("member6", 9));
+        //when
+        int resultCount = memberRepository.bulkAgePlus(20);
+
+        //then
+        assertThat(resultCount).isEqualTo(3);
+    }
 }
