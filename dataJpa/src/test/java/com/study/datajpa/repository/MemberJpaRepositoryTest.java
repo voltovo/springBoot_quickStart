c6ef063f696a74417a82a672b904c068b147bca9
sumanChoi <voltovo@naver.com>
2024-02-14 15:12:18 +0900

    feat:벌크성 수정 쿼리 순수 jpa 적용

--- dataJpa/src/test/java/com/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ dataJpa/src/test/java/com/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -76,4 +76,20 @@ class MemberJpaRepositoryTest {
         assertThat(totalCount).isEqualTo(6);
 
     }
+
+    @Test
+    public void bulkUpdate() throws Exception{
+      //given
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 19));
+        memberJpaRepository.save(new Member("member3", 20));
+        memberJpaRepository.save(new Member("member4", 21));
+        memberJpaRepository.save(new Member("member5", 40));
+        memberJpaRepository.save(new Member("member6", 9));
+      //when
+        int resultCount = memberJpaRepository.bulkAgePlus(20);
+
+        //then
+        assertThat(resultCount).isEqualTo(3);
+     }
 }
