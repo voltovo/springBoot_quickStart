671dd66e53d6d5b3bf542a728515cecab25ad614
sumanChoi <voltovo@naver.com>
2024-03-05 14:57:09 +0900

    feat: jpql method add

---
+++ querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,39 @@
+package study.querydsl.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void basicTest() throws Exception{
+      //given
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+        //when
+        Member findMember = memberJpaRepository.findById(member.getId()).get();
+
+        List<Member> findMember1 = memberJpaRepository.findAll();
+
+        List<Member> findMember2 = memberJpaRepository.findByUsername("member1");
+        //then
+        assertThat(findMember).isEqualTo(member);
+        assertThat(findMember1).containsExactly(member);
+        assertThat(findMember2).containsExactly(member);
+
+     }
+}
