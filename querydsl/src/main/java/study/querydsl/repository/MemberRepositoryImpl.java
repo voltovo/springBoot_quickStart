f92f70899af5aaf738195616d20edf43a07cfc64
sumanChoi <voltovo@naver.com>
2024-03-06 17:58:53 +0900

    feat:countQuery 최적화

--- querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -6,15 +6,18 @@ import static study.querydsl.entity.QTeam.team;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 public class MemberRepositoryImpl implements MemberRepositoryCustom{
     private final JPAQueryFactory queryFactory;
@@ -59,11 +62,11 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                 ageGoe(condition.getAgeGoe()), ageLoe(condition.getAgeLoe()))
             .offset(pageable.getOffset()).limit(pageable.getPageSize()).fetch();
 
-        long total = queryFactory.select(member).from(member).leftJoin(member.team, team)
+        JPAQuery<Member> countQuery = queryFactory.select(member).from(member).leftJoin(member.team, team)
             .where(usernameEq(condition.getUsername()), teamNameEq(condition.getTeamName()),
-                ageGoe(condition.getAgeGoe()), ageLoe(condition.getAgeLoe())).fetchCount();
-
-        return new PageImpl<>(content, pageable, total);
+                ageGoe(condition.getAgeGoe()), ageLoe(condition.getAgeLoe()));
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
+        //return new PageImpl<>(content, pageable, total);
     }
 
     private BooleanExpression usernameEq(String username) {
